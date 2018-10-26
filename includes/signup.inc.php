0d83a106a66c47fedf81dcd543c76633a430f914
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2018-10-26 12:55:09 +0100

    corrected php grammar

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -3,12 +3,12 @@ if (isset($_POST['registerButton'])) {
 
   require 'dbh.inc.php';
 
-  $name = $POST['nameField'];
-  $address = $POST['addressField'];
-  $postcode = $POST['pcField'];
-  $email = $POST['emailField'];
-  $password = $POST['passwordField'];
-  $cpassword = $POST['cpasswordField'];
+  $name = $_POST['nameField'];
+  $address = $_POST['addressField'];
+  $postcode = $_POST['pcField'];
+  $email = $_POST['emailField'];
+  $password = $_POST['passwordField'];
+  $cpassword = $_POST['cpasswordField'];
 
   //error handlers
   if(empty($name) || empty($address) || empty($postcode) || empty($email) || empty($password) || empty($cpassword)) {
