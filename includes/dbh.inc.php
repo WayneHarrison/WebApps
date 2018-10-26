0cdc6b1f7f9c4b3450c955a43dcdb4e4da125879
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2018-10-26 16:48:56 +0100

    changed connection properties.

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,14 @@
 <?php
+$con = mysqli_connect("ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com, cxvgnzbdx933nx2c, pzgz4db5bifleb6r, ejyc09067f68qv1j")
+or die("Connection Failed" .mysqli_error($con));
 
-$servername ="ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dBUsername ="cxvgnzbdx933nx2c";
-$dBPassword ="pzgz4db5bifleb6r";
-$dBName ="ejyc09067f68qv1j";
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+//$servername ="ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+//$dBUsername ="cxvgnzbdx933nx2c";
+//$dBPassword ="pzgz4db5bifleb6r";
+//$dBName ="ejyc09067f68qv1j";
+//$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
-if (!$conn) {
-  die("Connection Failed".mysqli_connect_error())
-}
+//if (!$conn) {
+//  die("Connection Failed".mysqli_connect_error())
+//}
 ?>
