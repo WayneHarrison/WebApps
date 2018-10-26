9eb5141569e78140741b1fbae3daee30070cb1dd
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2018-10-26 13:17:26 +0100

    aaaaaaa

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -11,8 +11,9 @@ if (isset($_POST['registerButton'])) {
   $cpassword = $_POST['cpassword'];
 
   //error handlers
-  if(empty($name) || empty($address) || empty($postcode) || empty($email) || empty($password) || empty($cpassword)) {
-    header("Location: ../register.php?error=emptyfields&name=".$name."&address=".$address."&postcode".$postcode."&email".$email);
+  if (empty($name) || empty($address) || empty($postcode) || empty($email) || empty($password) || empty($cpassword)){
+      header("Location: ../register.php?error=emptyfields&name=".$name."&address".$address."&postcode".$postcode."&email".$email);
+
   }
 
 }
