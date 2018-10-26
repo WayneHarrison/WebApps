b5c29695d97fe4104089168edf965932270b4214
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2018-10-26 17:05:20 +0100

    another fix

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,8 @@
 <?php
-<?php
+
 //connect to mysql database
 $con = mysqli_connect("ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "cxvgnzbdx933nx2c", " 	pzgz4db5bifleb6r", "ejyc09067f68qv1j") or die("Connection Failed" .
 mysqli_error($con));
-?>
 
 //$servername ="ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 //$dBUsername ="cxvgnzbdx933nx2c";
