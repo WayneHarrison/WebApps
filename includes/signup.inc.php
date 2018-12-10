497a4cf5576dd90d0c7cfb2eb7b99ee4b0de51b1
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2018-12-10 12:05:44 +0000

    error checking

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -15,8 +15,20 @@ if (isset($_POST['registerButton'])) {
       header("Location: ../register.php?error=emptyfields&name=".$name."&address".$address."&postcode".$postcode."&email".$email);
       exit();
   }
+  else if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z]*$/", $name)){
+    header("Location: ../register.php?error=invalidmailname=&address".$address."&postcode".$postcode);
+    exit();
+  }
   else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     header("Location: ../register.php?error=invalidmail&name=".$name."&address".$address."&postcode".$postcode);
+    exit();
+  }
+  else if (!preg_match("/^[a-zA-Z]*$/", $name)) {
+    header("Location: ../register.php?error=invalidname&address="."&address".$address."&postcode".$postcode."&email".$email);
+    exit();
+  }
+  else if ($password !== $cpassword) {
+    header("Location: ../register.php?error=passwordcheck&name=".$name."&address".$address."&postcode".$postcode."&email".$email);
     exit();
   }
 }
