fd2fbd3ef56ce239d8ca8f4bbd0bb28d499ce9be
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2018-12-10 13:35:45 +0000

    c

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -31,29 +31,13 @@ if (isset($_POST['registerButton'])) {
     header("Location: ../register.php?error=passwordcheck&name=".$name."&address".$address."&postcode".$postcode."&email".$email);
     exit();
   }
-      else {
-        $sql ="INSERT INTO user (userName, userAddress, userPostCode, userEmail, userPassword) VALUES (?, ?, ?, ?, ?)";
-        $stmt = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($stmt, $sql)){
-          header("Location: ../register.php?error=sqlerror2");
-          exit();
-        }
-        else {
-          $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-          mysqli_stmt_bind_param($stmt, "sssss",$name, $address, $postcode, $email, $hashedPwd);
-          mysqli_stmt_execute($stmt);
-          header("Location: ../register.php?success");
-          exit();
+      
         }
       }
     }
   }
-  mysqli_stmt_close($stmt);
-  mysqli_close($conn);
+
 }
-else {
-  header("Location: ../register.php");
-  exit();
 }
 
 ?>
