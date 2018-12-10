0ea97fabfe305249bc2d0ca67f1325b6cf3a1e95
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2018-12-10 12:48:35 +0000

    error checking and inserting values into db

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -31,7 +31,46 @@ if (isset($_POST['registerButton'])) {
     header("Location: ../register.php?error=passwordcheck&name=".$name."&address".$address."&postcode".$postcode."&email".$email);
     exit();
   }
-}
+  else {
+    $sql ="SELECT userEmail FROM user WHERE userEmail=?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+      header("Location: ../register.php?error=sqlerror");
+      exit();
+    }
+    else {
+      mysqli_stmt_bind_param($stmt, "s", $email);
+      mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);
+      $resultCheck = mysqli_stmt_num_rows($stmt);
+      if($resultCheck > 0 ) {
+        header("Location: ../register.php?error=emailTaken");
+        exit();
+      }
+      else {
+        $sql ="INSERT INTO users (userName, userAddress, userPostCode, userEmail, userPassword) VALUES (?, ?, ?, ?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)){
+          header("Location: ../register.php?error=sqlerror");
+          exit();
+        }
+        else {
+          $hashedPwd = password_hash($password, PASSWORD_DEFAULT)
 
+          mysqli_stmt_bind_param($stmt, "sssss",$name, $address, $postcode, $email, $hashedPwd);
+          mysqli_stmt_execute($stmt);
+          header("Location: ../register.php?success");
+          exit();
+        }
+      }
+    }
+  }
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
+}
+else {
+  header("Location: ../register.php");
+  exit();
+}
 
 ?>
