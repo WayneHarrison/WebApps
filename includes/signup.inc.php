38c32f4312fedcfcc898f6e5305f0d8829226ed8
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-01-10 11:15:14 +0000

    reintro code

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -31,14 +31,14 @@ if (isset($_POST['registerButton'])) {
               header("Location: ../register.php?error=passwordcheck&name=".$name."&address".$address."&postcode".$postcode."&email".$email);
               exit();
               }
-//  else {
-//
-//    $sql "SELECT userEmail FROM user where userEmail=?";
-//    $stmt = mysqli_stmt_init($con);
-//      if (!mysqli_stmt_prepare($stmt, $sql)){
-//      header("Location: ../register.php?error=sqlerror");
-//      exit();
-//      }
+  else {
+
+    $sql "SELECT userEmail FROM user where userEmail=?";
+    $stmt = mysqli_stmt_init($con);
+      if (!mysqli_stmt_prepare($stmt, $sql)){
+    header("Location: ../register.php?error=sqlerror");
+      exit();
+      }
 //        else {
 //        mysqli_stmt_bind_param($stmt, "s", $email);
 //        mysqli_stmt_execute($stmt);
