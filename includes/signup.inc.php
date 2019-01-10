d3194e988330a569cb67fac85806abe61c3909c1
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-01-10 12:34:07 +0000

    readded old code

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -41,35 +41,35 @@ if (isset($_POST['registerButton'])) {
       }
 
       }
-//        else {
-//        mysqli_stmt_bind_param($stmt, "s", $email);
-//        mysqli_stmt_execute($stmt);
-//        mysqli_stmt_store_result($stmt);
-//        $resultcheck = mysqli_stmt_num_rows($stmt);
-//          if ($resultcheck > 0){
-//          header("Location: ../register.php?error=emailtaken");
-//          exit();
-//            }
-//            else {
-//            $sql = "INSERT INTO user (userName, userAddress, userPostCode, userEmail, userPassword) VALUES (?, ?, ?, ?, ?)"
-//            $stmt = mysqli_stmt_init($conn);
-//              if (!mysqli_stmt_prepare($stmt, $sql)) {
-//              header("Location: ../register.php?error=sqlerror2");
-//              exit();
-//            }
-//                else{
-//
-//                  $hashedpwd= password_hash($password, PASSWORD_DEFAULT)
-//
-//                 mysqli_stmt_bind_param($stmt, "sssss",$name, $address, $postcode, $email, $hashedpwd );
-//                  mysqli_stmt_execute($stmt);
-//                  header("Location: ../register.php?signup=success");
-//                  exit();
-//                }
-//              }
-//            }
-//          mysqli_stmt_close($stmt);
-//          mysqli_close($conn);
+        else {
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        $resultcheck = mysqli_stmt_num_rows($stmt);
+          if ($resultcheck > 0){
+          header("Location: ../register.php?error=emailtaken");
+          exit();
+            }
+        else {
+            $sql = "INSERT INTO user (userName, userAddress, userPostCode, userEmail, userPassword) VALUES (?, ?, ?, ?, ?)"
+            $stmt = mysqli_stmt_init($conn);
+              if (!mysqli_stmt_prepare($stmt, $sql)) {
+              header("Location: ../register.php?error=sqlerror2");
+              exit();
+            }
+                else{
+
+                  $hashedpwd= password_hash($password, PASSWORD_DEFAULT)
+
+                  mysqli_stmt_bind_param($stmt, "sssss",$name, $address, $postcode, $email, $hashedpwd );
+                  mysqli_stmt_execute($stmt);
+                  header("Location: ../register.php?signup=success");
+                  exit();
+                }
+              }
+            }
+          mysqli_stmt_close($stmt);
+          mysqli_close($conn);
         }
 else {
   header("Location: ../register.php");
