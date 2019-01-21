0e768aee82f1108a755245bb6bd915ebbea51865
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-01-21 11:55:06 +0000

    ?

--- register.php
+++ register.php
@@ -39,7 +39,7 @@ session_start();
           <?php
             if(isset($_SESSION['usersID'])){
               echo'<li class="navigation-item">
-                <a class="navigation-link" href="includes/logout.inc.php">Logout</a>
+                <a class="navigation-link" href="includes/logout.inc.php" method="post">Logout</a>
               </li>'; }
               else {
                 echo'<li class="navigation-item">
