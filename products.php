734826d9c20df4e506468d0853a04eb0d07cb0f4
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-02-02 12:24:06 +0000

    added a fix

--- products.php
+++ products.php
@@ -67,9 +67,10 @@ session_start();
                   $query='SELECT * FROM car ORDER BY carID ASC';
 
                   $result = mysqli_query($conn, $query);
-                  if ($result) :
+                  if ($result):
                     if(mysqli_num_rows($result)>0):
                     while($product = mysqli_fetch_assoc($result)):
+                      print_r($product);
                       ?>
                       <div class="row">
                       <div class="column column-50">
