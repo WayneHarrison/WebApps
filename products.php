751be8b4dfce15487b19296fce7cc858d537d071
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-02-02 13:02:41 +0000

    ...........

--- products.php
+++ products.php
@@ -67,16 +67,17 @@ session_start();
                   $query='SELECT * FROM car ORDER BY carID ASC';
                   $result = mysqli_query($conn, $query);
 
-                  if ($result):
-                    if(mysqli_num_rows($result)>0):
-                      while($product = mysqli_fetch_assoc($result)):
+                if ($result){
+                   if(mysqli_num_rows($result)>0){
+                      while($product = mysqli_fetch_assoc($result)){
                         print_r($product);
 
             ?>
             <?php
-                  endif;
-                    endif;
-                      endwhile;
+                      endwhile}
+                   endif}
+                endif}
+
             ?>
 
                   </br>
