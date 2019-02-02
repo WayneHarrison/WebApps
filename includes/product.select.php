2a879cb7b1d8d276e6eb87479c575a406bb65246
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-02-02 11:57:22 +0000

    added missing semi colon

--- includes/product.select.php
+++ includes/product.select.php
@@ -1,8 +1,8 @@
 <?php
       require 'dbh.inc.php';
-      $query='SELECT * FROM car ORDER BY carID ASC'
+      $query='SELECT * FROM car ORDER BY carID ASC';
 
-      $result = mysqli_query($conn, $query)
+      $result = mysqli_query($conn, $query);
       if ($result) {
         if(mysqli_num_rows($result)>0)
         while($product = mysqli_fetch_assoc($result)){
