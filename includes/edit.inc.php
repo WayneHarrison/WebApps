80b4413fba53c585a373be363f6425f8122fb458
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-02-19 12:39:47 +0000

    changes to edit

--- includes/edit.inc.php
+++ includes/edit.inc.php
@@ -19,7 +19,7 @@ if (isset($_POST['editButton'])){
       exit();
     }
     else {
-      $sql = "INSERT INTO user (userAddress, userPostCode, userDOB, userPhone) VALUES (?, ?, ?, ?) WHERE userID = $uID";
+      $sql = "INSERT INTO user (userAddress, userPostCode, userDOB, userPhone) VALUES (?, ?, ?, ?) WHERE userID = ".$uID;
       $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("Location: ../profileedit.php?error=SQLError");
