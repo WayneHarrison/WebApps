c5b31ea044d66a4d0dc10cc9b7c1e032caa863e2
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-02-23 13:42:55 +0000

    Update register.php

--- register.php
+++ register.php
@@ -27,8 +27,7 @@ session_start();
     <nav class="navigation">
       <section class="container">
         <a class="navigation-title" href="index.php"><h1 class="title">WebApps</h1></a>
-          <ol class="navigation-list float-right">
-            <div class="dropdown">
+            <div class="dropdown navigation-list float-right">
               <p class="navigation-item">Menu</p>
               <!--Dropdown Navigation-->
                 <div class="dropdown-content">
@@ -58,7 +57,6 @@ session_start();
                       ?>
                 </div>
             </div>
-          </ol>
       </section>
     </nav>
     <!--Content Start-->
