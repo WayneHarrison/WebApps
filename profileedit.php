0bda9b8aeab2694cc9be7dc47e04cc8d9552b674
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-02-23 13:50:45 +0000

    Revert "Update profileedit.php"
    This reverts commit 13c09e7a47915bcf812c9c19baf702bc30ec938b.

--- profileedit.php
+++ profileedit.php
@@ -28,7 +28,8 @@ session_start();
     <nav class="navigation">
       <section class="container">
         <a class="navigation-title" href="index.php"><h1 class="title">WebApps</h1></a>
-            <div class="dropdown navigation-list float-right">
+          <ol class="navigation-list float-right">
+            <div class="dropdown">
               <p class="navigation-item">Menu</p>
               <!--Dropdown Navigation-->
                 <div class="dropdown-content">
@@ -58,6 +59,7 @@ session_start();
                       ?>
                 </div>
             </div>
+          </ol>
       </section>
     </nav>
     <!--Content Start-->
