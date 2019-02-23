261ea2004f2bf146cb3289fb0b254f11f12fce95
Wayne Harrison Jnr <h1395756@student.southstaffs.ac.uk>
2019-02-23 12:27:11 +0000

    cheech

--- profileedit.php
+++ profileedit.php
@@ -8,7 +8,7 @@ session_start();
     <title>Custom Imports</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui" charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui">
     <!-- Google Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <!--Icons-->
